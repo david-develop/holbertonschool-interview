86fd20cb3e670ac6c118197704682bce07bb9042
david-develop <924@holbertonschool.com>
2021-04-22 21:33:17 -0500

    upload code

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+    """Method that determines if all the boxes can be opened"""
+
+    boxes_number = [x for x in range(1, len(boxes))]
+    for i in range(len(boxes)):
+        for key in boxes[i]:
+            if key != i and key in boxes_number:
+                boxes_number.remove(key)
+        
+    return len(boxes_number) == 0
