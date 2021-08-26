1c5fc2a05565f17d25acbc44e63fc90f0b1bef6c
david-develop <924@holbertonschool.com>
2021-08-25 20:32:06 -0500

    betty style

--- 0x11-heap_sort/0-heap_sort.c
+++ 0x11-heap_sort/0-heap_sort.c
@@ -9,13 +9,13 @@
  */
 void swap(int *array, int first, int last, size_t size)
 {
-    int aux, value;
+	int aux, value;
 
-    value = array[first];
-    aux = array[last];
-    array[last] = value;
-    array[first] = aux;
-    print_array(array, size);
+	value = array[first];
+	aux = array[last];
+	array[last] = value;
+	array[first] = aux;
+	print_array(array, size);
 }
 
 /**
@@ -27,24 +27,24 @@ void swap(int *array, int first, int last, size_t size)
  */
 void heap_change(int *array, size_t size_1, size_t i, size_t size_2)
 {
-    size_t max, left, right;
+	size_t max, left, right;
 
-    max = i;
-    left = 2 * i + 1;
-    right = 2 * i + 2;
+	max = i;
+	left = 2 * i + 1;
+	right = 2 * i + 2;
 
-    if (left < size_1 && array[left] > array[max])
-        max = left;
+	if (left < size_1 && array[left] > array[max])
+		max = left;
 
-    if (right < size_1 && array[right] > array[max])
-        max = right;
+	if (right < size_1 && array[right] > array[max])
+		max = right;
 
-    if (max != i)
-    {
-        swap(array, i, max, size_2);
+	if (max != i)
+	{
+		swap(array, i, max, size_2);
 
-        heap_change(array, size_1, max, size_2);
-    }
+		heap_change(array, size_1, max, size_2);
+	}
 }
 /**
  * heap_sort - sorts an array of integers in ascending order using the Heap
@@ -54,16 +54,16 @@ void heap_change(int *array, size_t size_1, size_t i, size_t size_2)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i;
+	int i;
 
-    if (!array || size == 1)
-        return;
-    for (i = size / 2 - 1; i >= 0; i--)
-        heap_change(array, size, i, size);
+	if (!array || size == 1)
+		return;
+	for (i = size / 2 - 1; i >= 0; i--)
+		heap_change(array, size, i, size);
 
-    for (i = size - 1; i > 0; i--)
-    {
-        swap(array, 0, i, size);
-        heap_change(array, i, 0, size);
-    }
+	for (i = size - 1; i > 0; i--)
+	{
+		swap(array, 0, i, size);
+		heap_change(array, i, 0, size);
+	}
 }
