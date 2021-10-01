7590366e0c1d5dc528d3294bf72d4cb38c8d05bd
david-develop <david.peraltar@ab-inbev.com>
2021-09-30 21:30:33 -0500

    rotate optimized

--- 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x16-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,22 +6,7 @@ Moduel rotate_2d_matrix method
 
 def rotate_2d_matrix(matrix):
     """Method that rotate it 90 degrees clockwise of a n x n matrix"""
-    temp1 = matrix[0][0]
-    temp2 = matrix[0][-1]
-    temp3 = matrix[-1][-1]
-    temp4 = matrix[-1][0]
-
-    matrix[0][-1] = temp1
-    matrix[-1][-1] = temp2
-    matrix[-1][0] = temp3
-    matrix[0][0] = temp4
-
-    temp5 = matrix[0][1]
-    temp6 = matrix[1][-1]
-    temp7 = matrix[-1][1]
-    temp8 = matrix[1][0]
-
-    matrix[1][-1] = temp5
-    matrix[-1][1] = temp6
-    matrix[1][0] = temp7
-    matrix[0][1] = temp8
+    list_vals = list(zip(*matrix[::-1]))
+    for y in range(len(list_vals)):
+        for i in range(len(list_vals)):
+            matrix[y][i] = list_vals[y][i]
