7a9cad09de24be5bb9692ee894a8bc3f408de565
david-develop <david.peraltar@ab-inbev.com>
2021-10-13 12:33:37 -0500

    function that sorts an array of integers in ASC

---
+++ 0x18-merge_sort/README.md
@@ -0,0 +1,4 @@
+0x18. Merge Sort
+
+- Compilation
+gcc -Wall -Wextra -Werror -pedantic 0-main.c 0-merge_sort.c print_array.c -o merge
---
+++ 0x18-merge_sort/sort.h
@@ -0,0 +1,10 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+/*   -------------functions----------------     */
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+#endif
