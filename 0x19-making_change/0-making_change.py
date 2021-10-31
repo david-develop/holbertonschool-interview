85f302af9ed02ab8bfc779a18e7000d640019584
david-develop <david.peraltar@ab-inbev.com>
2021-10-31 17:44:41 -0500

    fix

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -10,25 +10,16 @@ def makeChange(coins, total):
     """
     if total == 0:
         return 0
-    if total < 0:
-        return -1
-    if len(coins) == 0:
+    if total < 0 or len(coins) == 0:
         return -1
     if len(coins) == 1 and total in coins:
         return 1
 
-    sorted_coins = sorted(coins, reverse=True)
-    while total >= 0:
+    nums = [float('inf') for x in range(total+1)]
+    nums[0] = 0
+    for denom in coins:
+        for amount in range(len(nums)):
+            if denom <= amount:
+                nums[amount] = min(nums[amount], 1 + nums[amount - denom])
+    return nums[total] if nums[total] != float('inf') else -1
 
-        for coin in sorted_coins:
-            if coin > total:
-                sorted_coins.pop(0)
-
-                return makeChange(sorted_coins, total)
-            if total - coin >= 0:
-                total -= coin
-
-                if coin > total:
-                    sorted_coins.pop(0)
-                return 1 + makeChange(sorted_coins, total)
-    return -1
